fb3cad15c297161c299e7ef5cb61f79e773376b0
Sergio Martins <a20871@alunos.ipca.pt>
2022-05-25 12:23:00 +0100

    Start

---
+++ templates/javascript.js
@@ -0,0 +1,105 @@
+var username
+var password
+
+function PedidoHTTP()
+{
+    const http = new XMLHttpRequest();
+    const url = "http://127.0.0.1:5000" + document.getElementById("domain").value + document.getElementById("domain2").value;
+    const method = document.getElementById("method").value;
+    const json = document.getElementById("json").value
+
+    http.open(method, url);
+    http.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.send(JSON.stringify(json));
+        
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4){
+            document.getElementById('mensagem').innerHTML = http.responseText
+
+            /*document.getElementById("mensagem").text = http.responseText
+            console.log("|" + http.responseType + "|")
+            var element = document.createElement('a');
+            element.setAttribute('href', 'data:attachment;charset=utf-8,' + encodeURIComponent(http.responseText));
+            element.setAttribute('download', "ola");
+
+            element.style.display = 'none';
+            document.body.appendChild(element);
+
+            element.click();
+
+            document.body.removeChild(element);*/
+        }
+    }
+}
+
+
+function FazerLogin()
+{
+    var socket = io();
+    const http = new XMLHttpRequest();
+    const url = "http://127.0.0.1:5000/login";
+    const method = "GET"
+    const json = {};
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    
+    http.open(method, url);
+    http.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.send(json);
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4)
+        {
+            document.write(http.response)
+            socket.emit('canais/entrar', {username: username});
+            socket.on('novamensagem', function(data) {
+                console.log(data['data'])
+                var div = document.getElementById('mensagens');
+                div.innerHTML += data['data'] + "<br>";
+            });
+        }
+    }
+}
+
+function Registar()
+{
+    const http = new XMLHttpRequest();
+    const url = "http://127.0.0.1:5000/utilizadores/criar_conta";
+    const method = "POST"
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    const json = {"username":username, "password":password};
+
+    http.open(method, url);
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+    http.send(JSON.stringify(json));
+
+    http.onreadystatechange = (e) => {
+        if (http.readyState === 4)
+            document.write(http.response)
+    }
+}
+
+function PedidoHTTPFicheiros()
+{
+    const http = new XMLHttpRequest();
+    const url = "http://127.0.0.1:5000" + document.getElementById("domain").value + document.getElementById("domain2").value;
+    const method = document.getElementById("method").value;
+    const json = document.getElementById("json").value
+
+    http.open(method, url, true);
+    http.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+    const file = document.getElementById("ficheiro")[0].files[0]
+    console.log(file)
+    
+    var formData = new FormData();
+    formData.append("ficheiro", file);
+    http.send(formData);
+        
+    http.onreadystatechange = (e) => {
+        document.getElementById("mensagem").text = http.responseText
+    }
+}
---
+++ utils.py
@@ -0,0 +1,56 @@
+import json, os
+import os.path, time, datetime
+
+months = {
+        'Jan': 1,
+        'Feb': 2,
+        'Mar': 3,
+        'Apr': 4,
+        'May': 5,
+        'Jun': 6,
+        'Jul': 7,
+        'Aug': 8,
+        'Sep': 9,
+        'Oct': 10,
+        'Nov': 11,
+        'Dec': 12,
+    }
+
+def escrever_ficheiro(filename, data):
+    path = "data/" + filename
+    with open(path, 'w') as file:
+        file.truncate(0)
+        json.dump(data, file)
+        file.close()
+
+def ler_ficheiro(filename):
+    path = "data/" + filename
+    try:
+        with open(path, 'r') as file:
+            try:
+                content = json.load(file)
+            except ValueError:
+                content = []
+    except IOError as e:
+        return []
+    file.close()
+    return content
+
+LIFE_TIME = 365 #days
+# returns True if exists, False otherwise checking its age
+def verificar_ficheiro(path):
+    if os.path.exists(path):
+        c = time.ctime(os.path.getctime(path))
+        day = c[8:10]
+        month = months[c[4:7]]
+        year = c[-4:]
+
+        file_date = datetime.datetime(int(year), int(month), int(day))
+        now = datetime.datetime.now()
+        difference = file_date - now
+
+        if abs(difference.days) > LIFE_TIME:
+            os.remove(path)
+            return False
+        return True
+    return False
